316181eefe648c5bba32654b6ed117c2364aa69a
TomaszWasilewski <40766689+TomaszWasilewski@users.noreply.github.com>
2023-03-07 08:18:53 +0100

    test5

--- liczby_binarne.py
+++ liczby_binarne.py
@@ -37,8 +37,7 @@
 
 '''
 sys.maxsize
-An TEST giving the maximum value a variable of type Py_ssize_t can take.
+An testtt giving the maximum value a variable of type Py_ssize_t can take.
 It’s usually 2**31 - 1 on a 32-bit platform and 2**63 - 1 on a 64-bit platform.
 '''
-
 
